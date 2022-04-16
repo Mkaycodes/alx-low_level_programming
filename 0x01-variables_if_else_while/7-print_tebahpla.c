2b2a7a6a6f63051e1d1b982f57867336a01d8c10
Mkaycodes <101532260+Mkaycodes@users.noreply.github.com>
2022-04-16 22:26:58 +0000

    Add files via upload

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Prints alphabets reversed
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
