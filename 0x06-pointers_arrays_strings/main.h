617166d1a45326cfd890b11ab3dac0604f324520
Mkaycodes <makafuikwawu14@gmail.com>
2022-07-13 13:04:04 -0700

    more arrays strings and  pointer 0x06

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -23,3 +27,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 int _putchar(char);
+
+#endif
