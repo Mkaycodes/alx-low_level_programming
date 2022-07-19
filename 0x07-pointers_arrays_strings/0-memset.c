2d8bb0538914be693a17241a71f200bd2e9b528b
Mkaycodes <makafuikwawu14@gmail.com>
2022-07-19 14:27:29 -0700

    tasks for even more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - prints buffer in hexa
- * @s: buffer
- * @b: write
- * @n: size
- * Return: Nothing.
+ * _memset - fills memory with a constant byte.
+ * @s: source string
+ * @b: the constant byte for filling
+ * @n: length of buffer
+ * Return: new string.
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
-	return (s);
+	 unsigned int i = 0;
+	 while (i < n)
+	 {
+		 *(s + i) = b;
+		 i++;
+	 }
+	 return (s);
 }
