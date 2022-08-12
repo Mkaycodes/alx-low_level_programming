4e2028ec5eb194f9abc263df9d07f7c138f345fd
Mkaycodes <101532260+Mkaycodes@users.noreply.github.com>
2022-08-12 12:30:49 +0000

    Update 1-list_len.c
    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,21 @@
+
 #include "lists.h"
+
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - returns the numbers of elements in a linked list_t list
+ * @h: pointer to the linked list_t
+ *
+ * Return: the number of elements in a linked list
  */
 
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
+	size_t n = 0;
 
-    nelem = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        nelem++;
-    }
-    return (nelem);
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
